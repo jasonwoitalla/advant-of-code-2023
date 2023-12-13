6a5a8ec5310aadf433b143836f52d73032995959
Jason Woitalla <jason.woitalla@gmail.com>
2023-12-12 20:37:02 -0600

    Day 12 Part 2

--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -3,11 +3,14 @@ import { parseLinesFromInput } from '../util/parseInput';
 export async function part1() {
     let sum = 0;
 
-    await parseLinesFromInput(__dirname + '/input.txt', line => {
+    await parseLinesFromInput(__dirname + '/example.txt', line => {
         let parts = line.split(' ');
         let groups: number[] = parts[1].split(',').map((number) => parseInt(number.trim()));
         
-        let allLines = generateLines(parts[0], parts[0].length);
+        let allLines = generateLines(parts[0]);
+        let numQuestionMarks = parts[0].split('').filter((char) => char === '?').length;
+
+        console.log(`Line ${parts[0]} has ${numQuestionMarks} question marks, which generated ${allLines.length} lines`);
 
         let numArrangements = 0;
         allLines.forEach((newLine) => {
@@ -24,7 +27,7 @@ export async function part1() {
     return sum;
 }
 
-function generateLines(line: string, lineLength: number): string[] {
+function generateLines(line: string): string[] {
     // Base Case
     if(line === '?') {
         let lines = [];
@@ -38,7 +41,7 @@ function generateLines(line: string, lineLength: number): string[] {
     }
 
     // Recursive case
-    let lines = generateLines(line.slice(1), lineLength);
+    let lines = generateLines(line.slice(1));
     if(line[0] === '.' || line[0] === '#') { // simple add character to line
         lines = lines.map((myLine) => line[0] + myLine);
     } else if(line[0] === '?') { // double the lines list to add a '.' and '#'
@@ -87,17 +90,15 @@ export async function part2() {
         let groups: number[] = parts[1].split(',').map((number) => parseInt(number.trim()));
         groups = Array(5).fill(groups).flat();
 
-        let allLines = generateLines(parts[0], parts[0].length);
+        parts[0] = unfoldLine(parts[0]);
 
-        let numArrangements = 0;
-        allLines.forEach((newLine) => {
-            let numGroups = getGroupsFromGears(newLine);
-            if(areNumbersEqual(groups, numGroups)) { // this is a possible combination
-                // console.log(`${newLine} has ${numGroups}`);
-                numArrangements += 1;
-            }
-        });
+        // console.log(`Expanded line: ${parts[0]} ${groups}`);
+
+        let table = new Map<string, number>();
+        let numArrangements = findNumArrangements(parts[0], groups, table);
+
         // console.log(`Line ${parts[0]} has ${numArrangements} arrangements`);
+
         sum += numArrangements;
     });
 
@@ -105,8 +106,78 @@ export async function part2() {
 }
 
 function unfoldLine(line: string) {
+    let newLine = '';
+    for(let i = 0; i < 5; i++) {
+        newLine += line;
+        if (i < 4)
+            newLine += '?';
+    }
 
-    return line;
+    return newLine;
+}
+
+function findNumArrangements(line: string, groups: number[], table: Map<string, number>): number {
+    // Base Case
+    line = trimLine(line);
+    if(line === '') { // no more characters, check if groups is empty
+        return groups.length === 0 ? 1 : 0;
+    } else if(groups.length === 0) { // no more groups, check if there is any blocked springs
+        return line.includes('#') ? 0 : 1;
+    }
+
+    // Memoization
+    let key = line + groups.toString();
+    if(table.has(key)) {
+        return table.get(key)!;
+    }
+
+    // Recursive case
+    // try and match the damaged springs with the current group number
+    // if we see a ? we can either add it to the current group or start a new group
+    let numArrangements = 0;
+    let damaged = countDamaged(line);
+    if(damaged === groups[0]) { // we have a possible arrangement
+        // console.log(`Found a possible arrangement: ${line} ${groups}`);
+        numArrangements += findNumArrangements(line.slice(damaged), groups.slice(1), table);
+    }
+
+    if(damaged === 0 && line.includes('?')) {
+        numArrangements += findNumArrangements(line.replace('?', '#'), groups, table);
+        numArrangements += findNumArrangements(line.replace('?', '.'), groups, table);
+    }
+
+    table.set(key, numArrangements);
+    return numArrangements;
+}
+
+function trimLine(line: string): string {
+    let start = 0;
+    let end = line.length - 1;
+
+    while(line[start] === '.') {
+        start += 1;
+    }
+
+    while(line[end] === '.') {
+        end -= 1;
+    }
+
+    return line.slice(start, end + 1);
+}
+
+function countDamaged(line: string): number {
+    let count = 0;
+    for(let i = 0; i < line.length; i++) {
+        if(line[i] === '#') {
+            count += 1;
+        } else if(line[i] === '.') {
+            return count; // valid damaged block
+        } else if(line[i] === '?') {
+            return 0; // can be either damaged or operational
+        }
+    }
+
+    return count;
 }
 
 /**
