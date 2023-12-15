19602b88d310d6c960ad1b8d63eba2b4b4fd7daa
Jason Woitalla <jason.woitalla@thrivent.com>
2023-12-15 12:02:26 -0600

    Day 15 Part 1 & 2

--- src/day15/day15.ts
+++ src/day15/day15.ts
@@ -1,17 +1,79 @@
 import { parseLinesFromInput } from '../util/parseInput';
 
 export async function part1() {
-    await parseLinesFromInput(__dirname + '/example.txt', line => {
+    let sum = 0;
+    await parseLinesFromInput(__dirname + '/input.txt', line => {
+        let steps = line.split(',');
+        for(let step of steps) {
+            sum += hash(step);
+            // console.log(`Step ${step} has hash value ${runningHash}`);
+        }
     });
 
-    return 0;
+    return sum;
+}
+
+function hash(msg: string) {
+    let hash = 0;
+
+    for(let i = 0; i < msg.length; i++) {
+        let ascii = msg.charCodeAt(i);
+
+        hash += ascii;
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
 }
 
 export async function part2() {
-    await parseLinesFromInput(__dirname + '/example.txt', line => {
+    let hashmap: Map<number, Map<string, number>> = new Map();
+    let numBoxes = 256;
+    for(let i = 0; i < numBoxes; i++) {
+        hashmap.set(i, new Map());
+    }
+
+    let sum = 0;
+    await parseLinesFromInput(__dirname + '/input.txt', line => {
+        let steps = line.split(',');
+        for(let step of steps) {
+            let label = '';
+            let focalLength = '';
+            let remove = false;
+            if(step.includes('=')) { // add / replace operation
+                [label, focalLength] = step.split('=');
+            } else { // remove operation
+                label = step.substring(0, step.length - 1);
+                remove = true;
+            }
+
+            let boxNum = hash(label);
+            if(remove && hashmap.get(boxNum)?.has(label)) { // we have this label
+                hashmap.get(boxNum)?.delete(label);
+            } else if(!remove) {
+                hashmap.get(boxNum)?.set(label, parseInt(focalLength));
+            }
+            // console.log(`The box number for label: ${label} is ${boxNum}`);
+            // hashmap.forEach((val, key) => {
+            //     console.log(`Box ${key} is ${JSON.stringify([...val])}`);
+            // });
+        }
     });
 
-    return 0;
+    hashmap.forEach((val, key) => {
+        console.log(`Box ${key} is ${JSON.stringify([...val])}`);
+    });
+
+    hashmap.forEach((val, key) => {
+        let position = 1;
+        val.forEach((focalLength, label) => {
+            sum += (key + 1) * position * focalLength;
+            position += 1;
+        });
+    });
+
+    return sum;
 }
 
 /**
