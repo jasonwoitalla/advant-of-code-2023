9a60e271136529e53272ec8a915c45997849c65b
Jason Woitalla <jason.woitalla@gmail.com>
2023-12-18 14:45:45 -0600

    Day 16

--- src/day16/day16.ts
+++ src/day16/day16.ts
@@ -1,20 +1,323 @@
+import { Console } from 'console';
 import { parseLinesFromInput } from '../util/parseInput';
 
+type Range = {
+    start: [number, number];
+    end: [number, number];
+}
+
+// a tuple that represents the direction and position of a beam
+type Beam = {
+    dir: [number, number];
+    pos: [number, number];
+    ranges: Range[];
+}
+
+var width = 0;
+var height = 0;
+var lastCovered = 0;
+var coveredTiles: Set<string> = new Set();
+
 export async function part1() {
-    await parseLinesFromInput(__dirname + '/example.txt', line => {
+    let grid: string[][] = [];
+
+    await parseLinesFromInput(__dirname + '/input.txt', line => {
+        if(height === 0) {
+            width = line.length;
+        }
+
+        grid.push(line.split(''));
+        height += 1;
     });
 
-    return 0;
+    var beams: Beam[] = [{ dir: [1, 0], pos: [0, 0], ranges: [] }];
+    coveredTiles.add('0,0');
+
+    // Reflections
+    if(grid[beams[0].pos[1]][beams[0].pos[0]] === '/') {
+        beams[0].dir = [-beams[0].dir[1], -beams[0].dir[0]];
+    } else if(grid[beams[0].pos[1]][beams[0].pos[0]] === '\\') {
+        beams[0].dir = [beams[0].dir[1], beams[0].dir[0]];
+    }
+
+    let process = true;
+    let beamsToRemove: number[] = [];
+    while(process) {
+        [process, beamsToRemove] = processBeams(beams, grid);
+        let oldLength = beams.length;
+
+        // Find and remove any overlapping beams
+        for(let beam of beams) {
+            for(let otherBeam of beams) {
+                if(beam === otherBeam) {
+                    continue;
+                }
+
+                if(beam.pos[0] === otherBeam.pos[0] && beam.pos[1] === otherBeam.pos[1] && 
+                    beam.dir[0] === otherBeam.dir[0] && beam.dir[1] === otherBeam.dir[1]) {
+                    beamsToRemove.push(beams.indexOf(otherBeam));
+                }
+            }
+        }
+
+        beamsToRemove.sort((a, b) => b - a);
+        for(let i = 0; i < beamsToRemove.length; i++) {
+            beams.splice(beamsToRemove[i], 1);
+        }
+
+        // await delay(1000);
+        // console.log('-------------------');
+        // drawGrid(grid);
+        // console.log(`Length was ${oldLength}`);
+        // console.log(`Removed ${beamsToRemove.length} beams`);
+        // console.log(`Processing ${beams.length} beams`);
+        // console.log('');
+    }
+
+    // drawGrid(grid);
+
+    let numTiles = 0;
+    for(let tile of coveredTiles) {
+        let parts = tile.split(',');
+        let x = parseInt(parts[0]);
+        let y = parseInt(parts[1]);
+        if(x >= 0 && x < width && y >= 0 && y < height) {
+            numTiles += 1;
+        }
+    }
+
+    return numTiles;
+}
+
+function drawGrid(grid: string[][]) {
+    let drawGrid: string[][] = grid.map(row => row.map(cell => cell));
+    let numTiles = 0;
+    for(let tile of coveredTiles) {
+        let parts = tile.split(',');
+        let x = parseInt(parts[0]);
+        let y = parseInt(parts[1]);
+        if(x >= 0 && x < width && y >= 0 && y < height) {
+            drawGrid[y][x] = 'X';
+            numTiles += 1;
+        }
+    }
+
+    // Print out the grid
+    for(let y = 0; y < height; y++) {
+        let line = '';
+        for(let x = 0; x < width; x++) {
+            line += drawGrid[y][x];
+        }
+        console.log(line);
+    }
+    console.log(`Covered ${numTiles} tiles`);
+}
+
+function processBeams(beams: Beam[], grid: string[][]): [boolean, number[]] {
+    // console.log(`Processing ${beams.length} beams`);
+    let beamsToRemove: number[] = [];
+    for(let beam of beams) {
+        if(beam.pos[0] < 0 || beam.pos[0] >= width || beam.pos[1] < 0 || beam.pos[1] >= height) { // beam has left the grid
+            beamsToRemove.push(beams.indexOf(beam));
+            continue;
+        }
+
+        if(grid[beam.pos[1]][beam.pos[0]] === '/' || grid[beam.pos[1]][beam.pos[0]] === '\\') {
+            beam.pos = [beam.pos[0] + beam.dir[0], beam.pos[1] + beam.dir[1]];
+        }
+
+        let range: Range = { start: [beam.pos[0], beam.pos[1]], end: [0, 0] };
+
+        // console.log(`Processing beam at ${beam.pos[0]},${beam.pos[1]} in direction ${beam.dir[0]},${beam.dir[1]}`);
+        // console.log(`     With symbol ${grid[beam.pos[1]][beam.pos[0]]}`);
+
+        while(canMove(beam.pos, beam.dir, grid)) {
+            beam.pos = [beam.pos[0] + beam.dir[0], beam.pos[1] + beam.dir[1]];
+        }
+
+        if(beam.pos[0] < 0 || beam.pos[0] >= width || beam.pos[1] < 0 || beam.pos[1] >= height) { // beam has left the grid
+            range.end = [beam.pos[0], beam.pos[1]];
+            beam.ranges.push(range);
+            let startX = Math.min(range.start[0], range.end[0]);
+            let endX = Math.max(range.start[0], range.end[0]);
+            let startY = Math.min(range.start[1], range.end[1]);
+            let endY = Math.max(range.start[1], range.end[1]);
+
+            for(let x = startX; x <= endX; x++) {
+                for(let y = startY; y <= endY; y++) {
+                    coveredTiles.add(`${x},${y}`);
+                }
+            }
+
+            beamsToRemove.push(beams.indexOf(beam));
+            continue;
+        }
+
+        let isVertical = beam.dir[0] === 0;
+        if(isVertical && grid[beam.pos[1]][beam.pos[0]] === '-') { // split beam
+            // console.log(`Splitting horizontally beam at ${beam.pos[0]},${beam.pos[1]}`);
+            beam.dir = [1, 0];
+            beams.push({ dir: [-1, 0], pos: [beam.pos[0], beam.pos[1]], ranges: [] });
+        } else if(!isVertical && grid[beam.pos[1]][beam.pos[0]] === '|') { // split beam
+            // console.log(`Splitting vertically beam at ${beam.pos[0]},${beam.pos[1]}`);
+            beam.dir = [0, 1];
+            beams.push({ dir: [0, -1], pos: [beam.pos[0], beam.pos[1]], ranges: [] });
+        }
+
+        // Reflections
+        if(grid[beam.pos[1]][beam.pos[0]] === '/') {
+            beam.dir = [-beam.dir[1], -beam.dir[0]];
+        } else if(grid[beam.pos[1]][beam.pos[0]] === '\\') {
+            beam.dir = [beam.dir[1], beam.dir[0]];
+        }
+
+        range.end = [beam.pos[0], beam.pos[1]];
+        beam.ranges.push(range);
+
+        let startX = Math.min(range.start[0], range.end[0]);
+        let endX = Math.max(range.start[0], range.end[0]);
+        let startY = Math.min(range.start[1], range.end[1]);
+        let endY = Math.max(range.start[1], range.end[1]);
+
+        for(let x = startX; x <= endX; x++) {
+            for(let y = startY; y <= endY; y++) {
+                coveredTiles.add(`${x},${y}`);
+            }
+        }
+    }
+
+    if(coveredTiles.size === lastCovered) { // end condition
+        return [false, beamsToRemove];
+    }
+
+    lastCovered = coveredTiles.size;
+    return [true, beamsToRemove];
+}
+
+function canMove(pos: [number, number], dir: [number, number], grid: string[][]) {
+    // console.log(`Checking if can move from ${pos[0]},${pos[1]} in direction ${dir[0]},${dir[1]}`);
+    let isVertical = dir[0] === 0;
+    let splitterSymbol = isVertical ? '-' : '|';
+
+    return pos[0] >= 0 && pos[0] < width && pos[1] >= 0 && pos[1] < height && grid[pos[1]][pos[0]] !== splitterSymbol
+        && grid[pos[1]][pos[0]] !== '/' && grid[pos[1]][pos[0]] !== '\\';
+}
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
 export async function part2() {
-    await parseLinesFromInput(__dirname + '/example.txt', line => {
+    let grid: string[][] = [];
+
+    await parseLinesFromInput(__dirname + '/input.txt', line => {
+        if(height === 0) {
+            width = line.length;
+        }
+
+        grid.push(line.split(''));
+        height += 1;
     });
 
-    return 0;
+    let answers: number[] = [];
+    let startingDir: [number, number] = [0, 0];
+
+    // From the top row going south
+    startingDir = [0, 1];
+    for(let x = 0; x < width; x++) {
+        answers.push(findNumTiles(grid, [x, 0], startingDir));
+    }
+    console.log('Done with top row');
+
+    // From the bottom row going north
+    startingDir = [0, -1];
+    for(let x = 0; x < width; x++) {
+        answers.push(findNumTiles(grid, [x, height - 1], startingDir));
+    }
+    console.log('Done with bottom row');
+
+    // From the left column going east
+    startingDir = [1, 0];
+    for(let y = 0; y < height; y++) {
+        answers.push(findNumTiles(grid, [0, y], startingDir));
+    }
+    console.log('Done with left column');
+
+    // From the right column going west
+    startingDir = [-1, 0];
+    for(let y = 0; y < height; y++) {
+        answers.push(findNumTiles(grid, [width - 1, y], startingDir));
+    }
+    console.log('Done with right column');
+
+    return Math.max(...answers);
+}
+
+function findNumTiles(grid: string[][], startPos: [number, number], startDir: [number, number]) {
+    var beams: Beam[] = [{ dir: startDir, pos: startPos, ranges: [] }];
+
+    coveredTiles = new Set();
+    coveredTiles.add(`${startPos[0]},${startPos[1]}`);
+
+    // Reflections
+    if(grid[beams[0].pos[1]][beams[0].pos[0]] === '/') {
+        beams[0].dir = [-beams[0].dir[1], -beams[0].dir[0]];
+    } else if(grid[beams[0].pos[1]][beams[0].pos[0]] === '\\') {
+        beams[0].dir = [beams[0].dir[1], beams[0].dir[0]];
+    }
+
+    let process = true;
+    let beamsToRemove: number[] = [];
+    while(process) {
+        [process, beamsToRemove] = processBeams(beams, grid);
+
+        // Find and remove any overlapping beams
+        for(let beam of beams) {
+            for(let otherBeam of beams) {
+                if(beam === otherBeam) {
+                    continue;
+                }
+
+                if(beam.pos[0] === otherBeam.pos[0] && beam.pos[1] === otherBeam.pos[1] && 
+                    beam.dir[0] === otherBeam.dir[0] && beam.dir[1] === otherBeam.dir[1]) {
+                    beamsToRemove.push(beams.indexOf(otherBeam));
+                }
+            }
+        }
+
+        beamsToRemove.sort((a, b) => b - a);
+        for(let i = 0; i < beamsToRemove.length; i++) {
+            beams.splice(beamsToRemove[i], 1);
+        }
+
+        // await delay(1000);
+        // console.log('-------------------');
+        // drawGrid(grid);
+        // console.log(`Length was ${oldLength}`);
+        // console.log(`Removed ${beamsToRemove.length} beams`);
+        // console.log(`Processing ${beams.length} beams`);
+        // console.log('');
+    }
+
+    // drawGrid(grid);
+
+    let numTiles = 0;
+    for(let tile of coveredTiles) {
+        let parts = tile.split(',');
+        let x = parseInt(parts[0]);
+        let y = parseInt(parts[1]);
+        if(x >= 0 && x < width && y >= 0 && y < height) {
+            numTiles += 1;
+        }
+    }
+
+    return numTiles;
 }
 
 /**
  * Attempted Answers:
+ * 
+ * Part 1:
+ * 6920 (too low)
  * 
  */
--- src/day17/day17.ts
+++ src/day17/day17.ts
@@ -1,10 +1,181 @@
 import { parseLinesFromInput } from '../util/parseInput';
 
+import colors from 'colors';
+
+var height = 0;
+var width = 0;
+
+const RIGHT: [number, number] = [1, 0];
+const LEFT: [number, number] = [-1, 0];
+const UP: [number, number] = [0, -1];
+const DOWN: [number, number] = [0, 1];
+
+class Graph {
+    vertices: number;
+    edges: [number, number][][];
+
+    constructor(vertices: number) {
+        this.vertices = vertices;
+        this.edges = new Array<[number, number][]>(vertices);
+        for (let i = 0; i < vertices; i++) {
+            this.edges[i] = [];
+        }
+    }
+
+    addEdge(source: number, destination: number, weight: number) {
+        this.edges[source].push([destination, weight]);
+    }
+
+    shortestPath(source: number, dest: number = -1): [number, number[]] | undefined {
+        let priorityQueue: {weight: number, pos: number, dir: [number, number], straight: number}[] = [];
+        let dist = new Array<number>(this.vertices).fill(2147483647);
+        let prev = new Array<number>(this.vertices).fill(-1);
+        let visited: boolean[] = new Array<boolean>(this.vertices).fill(false);
+
+        priorityQueue.push({weight: 0, pos: source, dir: DOWN, straight: 0});
+        priorityQueue.push({weight: 0, pos: source, dir: RIGHT, straight: 0});
+        dist[source] = 0;
+
+        while(priorityQueue.length > 0) {
+            let current = priorityQueue[0];
+            priorityQueue.shift();
+
+            if(visited[current.pos]) continue; // Already visited this node
+            visited[current.pos] = true;
+
+            if(current.pos === dest) {
+                return [current.weight, prev];
+            }
+
+            let [curX, curY] = getCoordinates(current.pos);
+            console.log(`Currently at {weight: ${current.weight}, pos: [${curX},${curY}], dir: ${current.dir}, straight: ${current.straight}}`);
+
+            for(let i = 0; i < this.edges[current.pos].length; i++) {
+                let [next, weight]: [number, number] = this.edges[current.pos][i];
+
+                let [nextX, nextY] = getCoordinates(next);
+                let dir: [number, number] = [nextX - curX, nextY - curY];
+                let distance = dist[current.pos] + weight;
+
+                let isStraight = dir[0] === current.dir[0] && dir[1] === current.dir[1];
+
+                console.log(`    Checking {weight: ${distance}, pos: [${nextX},${nextY}], dir: ${dir}, straight: ${current.straight + 1}} is straight=${isStraight}`);
+
+                if(dist[next] > distance && isStraight && current.straight < 2) {
+                    dist[next] = distance;
+                    prev[next] = current.pos;
+                    priorityQueue.push({weight: distance, pos: next, dir: dir, straight: current.straight + 1});
+                } else if(dist[next] > distance && !isStraight) {
+                    dist[next] = distance;
+                    prev[next] = current.pos;
+                    priorityQueue.push({weight: distance, pos: next, dir: dir, straight: 0});
+                }
+
+                priorityQueue.sort((a, b) => {
+                    return a.weight - b.weight;
+                });
+            }
+        }
+
+        if(dest !== -1) {
+            return [dist[dest], prev];
+        }
+    }
+}
+
 export async function part1() {
+    let grid: string[][] = [];
     await parseLinesFromInput(__dirname + '/example.txt', line => {
+        if(height === 0) {
+            width = line.length;
+        }
+        grid.push(line.split(''));
+
+        height += 1;
     });
 
-    return 0;
+    // Build graph
+    let graph = new Graph(width * height);
+    for(let y = 0; y < height; y++) {
+        for(let x = 0; x < width; x++) {
+            // Connect to right, left, above and below
+            if(x < width - 1) {
+                graph.addEdge(y * width + x, y * width + x + 1, parseInt(grid[y][x + 1]));
+            }
+
+            if(x > 0) {
+                graph.addEdge(y * width + x, y * width + x - 1, parseInt(grid[y][x - 1]));
+            }
+
+            if(y < height - 1) {
+                graph.addEdge(y * width + x, (y + 1) * width + x, parseInt(grid[y + 1][x]));
+            }
+
+            if(y > 0) {
+                graph.addEdge(y * width + x, (y - 1) * width + x, parseInt(grid[y - 1][x]));
+            }
+        }
+    }
+
+    // Find shortest path
+    let bottomRight = (height - 1) * width + (width - 1);
+    let [answer, path]: [number, number[]] = graph.shortestPath(0, bottomRight)!;
+
+    // Draw shortest path on grid
+    let tempGrid: string[][] = grid.map((row) => row.map((e) => e));
+
+    let prev = bottomRight;
+    while(prev != 0) {
+        let [x, y] = getCoordinates(prev);
+        tempGrid[y][x] = colors.bold.red('X');
+        prev = path[prev];
+    }
+    tempGrid[0][0] = colors.bold.red('X');
+
+    for(let y = 0; y < height; y++) {
+        console.log(tempGrid[y].join(''));
+    }
+
+    return answer;
+}
+
+function getNumInDirection(path: number[], destPos: number) {
+    let pathCopy = path.map((e) => e);
+
+    let currentPos: number = pathCopy.pop()!;
+    let [currentX, currentY] = getCoordinates(currentPos);
+    let [destX, destY] = getCoordinates(destPos);
+
+    let deltaX = destX - currentX;
+    let deltaY = destY - currentY;
+
+    let numberInDirection = 0;
+    let prevPos = pathCopy.pop();
+
+    while(prevPos !== undefined) {
+        let [lastX, lastY] = getCoordinates(prevPos);
+
+        if(lastX === currentX - deltaX && lastY === currentY - deltaY) {
+            currentX = lastX;
+            currentY = lastY;
+            numberInDirection += 1;
+        } else {
+            // console.log(`   [${lastX}, ${lastY}] to [${currentX}, ${currentY}] has ${numberInDirection} points in its direction`);
+            break;
+        }
+
+        prevPos = pathCopy.pop();
+    }
+
+    // console.log(`Checking ${currentPos} to ${newPos} has a path of ${JSON.stringify(path)} with ${numberInDirection} in direction ${deltaX}, ${deltaY}`);
+    return numberInDirection;
+}
+
+function getCoordinates(num: number): [number, number] {
+    let x = num % width;
+    let y = Math.floor(num / width);
+
+    return [x, y];
 }
 
 export async function part2() {
