22a0634b4da64dddf0830036577631506fbc733f
Jason Woitalla <jason.woitalla@gmail.com>
2023-12-20 13:23:53 -0600

    Day 19 Part 1

--- src/day19/day19.ts
+++ src/day19/day19.ts
@@ -21,14 +21,48 @@ type Condition = {
     val: number;
 }
 
-export async function part1() {
-    await parseLinesFromInput(__dirname + '/example.txt', line => {
+type Input = {
+    x: number; a: number; s: number; m:number;
+}
 
+export async function part1() {
+    let parsingFunctions = true;
+    let functions = new Map<string, Function>();
+    let inputs: Input[] = [];
+    await parseLinesFromInput(__dirname + '/input.txt', line => {
+        if(line === '') {
+            parsingFunctions = false;
+            return;
+        }
+
+        if(parsingFunctions) {
+            let func = parseFunction(line);
+            functions.set(func.name, func);
+        } else {
+            let input = parseInput(line);
+            inputs.push(input);
+        }
     });
 
-    let functions = new Map<string, Function>();
+    console.log('Functions: ');
+    functions.forEach((value, key) => {
+        console.log(key + ': ' + JSON.stringify(value));
+    });
 
-    return 0;
+    let approved: number = 0;
+
+    for(let input of inputs) {
+        let result: string = evaluate(input, functions);
+        if(result === APPROVED) {
+            console.log('Approved: ' + JSON.stringify(input));
+            approved += input.x;
+            approved += input.a;
+            approved += input.s;
+            approved += input.m;
+        }
+    }
+
+    return approved;
 }
 
 function parseFunction(line: string): Function {
@@ -37,7 +71,8 @@ function parseFunction(line: string): Function {
 
     let conditions = parts[1].split(',');
     for(let i = 0; i < conditions.length - 1; i++) {
-        let condition: Condition = {arg: conditions[i].charAt(0), op: conditions[i].charAt(1) as Operator, val: 0};
+        let operation: Operator = conditions[i].charAt(1) === '>' ? Operator.GT : Operator.LT;
+        let condition: Condition = {arg: conditions[i].charAt(0), op: operation, val: 0};
         let vals = conditions[i].substring(2).split(':');
         condition.val = parseInt(vals[0]);
 
@@ -47,12 +82,92 @@ function parseFunction(line: string): Function {
         out.returns.push(result);
     }
 
+    out.default = conditions[conditions.length - 1].replace('}', '').trim();
+
     return out;
 }
 
+function parseInput(line: string): Input {
+    let out: Input = {x: 0, a: 0, s: 0, m: 0};
+    line = line.replace('{', '').replace('}', '');
+
+    let parts = line.split(',');
+    for(let part of parts) {
+        let vals = part.split('=');
+        let val = parseInt(vals[1]);
+
+        switch(vals[0].trim()) {
+            case 'x': out.x = val; break;
+            case 'a': out.a = val; break;
+            case 's': out.s = val; break;
+            case 'm': out.m = val; break;
+        }
+    }
+
+    return out;
+}
+
+function evaluate(input: Input, functions: Map<string, Function>): string {
+    let result = '';
+    let funcName = 'in';
+    while(result !== APPROVED && result !== REJECTED) {
+        let fcn = functions.get(funcName);
+        if(fcn === undefined) {
+            console.log('Function ' + funcName + ' not found!');
+            return REJECTED;
+        }
+        result = _evaluate(input, fcn);
+        funcName = result;
+    }
+
+    return result;
+}
+
+function _evaluate(input: Input, fcn: Function): string {
+    for(let i = 0; i < fcn.conditions.length; i++) {
+        switch(fcn.conditions[i].arg) {
+            case 'x': if(compare(input.x, fcn.conditions[i].op, fcn.conditions[i].val)) return fcn.returns[i]; break;
+            case 'a': if(compare(input.a, fcn.conditions[i].op, fcn.conditions[i].val)) return fcn.returns[i]; break;
+            case 's': if(compare(input.s, fcn.conditions[i].op, fcn.conditions[i].val)) return fcn.returns[i]; break;
+            case 'm': if(compare(input.m, fcn.conditions[i].op, fcn.conditions[i].val)) return fcn.returns[i]; break;
+        }
+    }
+
+    return fcn.default;
+}
+
+function compare(val: number, op: Operator, compare: number): boolean {
+    switch(op) {
+        case Operator.GT: return val > compare;
+        case Operator.LT: return val < compare;
+    }
+}
+
 export async function part2() {
+    let parsingFunctions = true;
+    let functions = new Map<string, Function>();
     await parseLinesFromInput(__dirname + '/example.txt', line => {
+        if(line === '') {
+            parsingFunctions = false;
+            return;
+        }
+
+        if(parsingFunctions) {
+            let func = parseFunction(line);
+            functions.set(func.name, func);
+        }
     });
+
+    console.log('Functions: ');
+    functions.forEach((value, key) => {
+        console.log(key + ': ' + JSON.stringify(value));
+    });
+
+    // Step 1: Simplify functions
+
+    // Step 2: Find the ranges of approved values for each function
+
+    // Step 3: Add up the ranges to find the total number of approved values
 
     return 0;
 }
